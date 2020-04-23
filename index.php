bdab11187914b3456e7b278d46034aa80cbd9379
Tauseef Shah <tauseefsshah@gmail.com>
2020-04-23 10:25:02 +0530

    Initial commit - VPS default page for codefuel

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Codefuel - VPS</title>
+
+    <!--META DATA-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+	<meta name="author" content="Tauseef Shah, Codefuel" />
+
+    <!--CSS FILES-->
+    <link rel="icon" type="image/png" href="img/logo.png">
+    <link rel="stylesheet" href="css/style.css"></link>
+</head>
+<body>
+
+<div>
+	<img src="img/logo.png" width="250px" height="auto" class="logo" alt="Codefuel - VPS" />
+</div>
+
+<div class="footer">
+	<p>&copy; <?php echo date("Y"); ?> Codefuel. All rights reserved.</p>
+</div>
+
+</body>
+</html>
